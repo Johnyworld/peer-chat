8da054c91370b389a666a98f7b6756e5a8690996
johnyworld <johnyworld@naver.com>
2024-01-12 13:01:13 +0900

    Publishing streams and handling remote users

--- mumble2/js/room_rtc.js
+++ mumble2/js/room_rtc.js
@@ -24,6 +24,9 @@ const joinRoomInit = async () => {
   client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
   await client.join(APP_ID, roomId, token, uid);
 
+  client.on('user-published', handleUserPublished);
+  client.on('user-left', handleUserLeft);
+
   joinStream();
 };
 
@@ -37,6 +40,35 @@ const joinStream = async () => {
   document.getElementById('streams__container').insertAdjacentHTML('beforeend', player);
 
   localTracks[1].play(`user-${uid}`);
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+const handleUserPublished = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+
+  await client.subscribe(user, mediaType);
+
+  const player = document.getElementById(`user-container-${user.uid}`);
+  if (player === null) {
+    const player = `<div class="video__container" id="user-container-${user.uid}">
+      <div class="video-player" id="user-${user.uid}"></div>
+    </div>`;
+
+    document.getElementById('streams__container').insertAdjacentHTML('beforeend', player);
+  }
+
+  if (mediaType === 'video') {
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === 'audio') {
+    user.audioTrack.play();
+  }
+};
+
+const handleUserLeft = async user => {
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
 };
 
 joinRoomInit();
