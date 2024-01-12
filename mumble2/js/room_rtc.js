2a132177f3e8f35ef6ca1d896b18d95be091a84f
johnyworld <johnyworld@naver.com>
2024-01-12 14:45:54 +0900

    Screen sharing

--- mumble2/js/room_rtc.js
+++ mumble2/js/room_rtc.js
@@ -20,6 +20,9 @@ if (!roomId) {
 let localTracks = [];
 let remoteUsers = {};
 
+let localScreenTracks;
+let sharingScreen = false;
+
 const joinRoomInit = async () => {
   client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
   await client.join(APP_ID, roomId, token, uid);
@@ -50,6 +53,22 @@ const joinStream = async () => {
 
   localTracks[1].play(`user-${uid}`);
   await client.publish([localTracks[0], localTracks[1]]);
+};
+
+const switchToCamera = async () => {
+  const player = `<div class="video__container" id="user-container-${uid}">
+    <div class="video-player" id="user-${uid}"></div>
+  </div>`;
+  displayFrame.insertAdjacentHTML('beforeend', player);
+
+  await localTracks[0].setMuted(true);
+  await localTracks[1].setMuted(true);
+
+  document.getElementById('mic-btn').classList.remove('active');
+  document.getElementById('screen-btn').classList.remove('active');
+
+  localTracks[1].play(`user-${uid}`);
+  await client.publish([localTracks[1]]);
 };
 
 const handleUserPublished = async (user, mediaType) => {
@@ -122,7 +141,54 @@ const toggleCamera = async e => {
   }
 };
 
+const toggleScreen = async e => {
+  const screenButton = e.currentTarget;
+  const cameraButton = document.getElementById('camera-btn');
+
+  if (!sharingScreen) {
+    sharingScreen = true;
+
+    screenButton.classList.add('active');
+    cameraButton.classList.remove('active');
+    cameraButton.style.display = 'none';
+
+    localScreenTracks = await AgoraRTC.createScreenVideoTrack();
+
+    document.getElementById(`user-container-${uid}`).remove();
+    displayFrame.style.display = 'block';
+
+    const player = `<div class="video__container" id="user-container-${uid}">
+      <div class="video-player" id="user-${uid}"></div>
+    </div>`;
+
+    displayFrame.insertAdjacentHTML('beforeend', player);
+    document.getElementById(`user-container-${uid}`).addEventListener('click', expandVideoFrame);
+
+    userIdInDisplayFrame = `user-container-${uid}`;
+    localScreenTracks.play(`user-${uid}`);
+
+    await client.unpublish([localTracks[1]]);
+    await client.publish([localScreenTracks]);
+
+    const videoFrames = document.getElementsByClassName('video__container');
+    for (let i = 0; i < videoFrames.length; i++) {
+      if (videoFrames[i].id != userIdInDisplayFrame) {
+        videoFrames[i].style.height = '100px';
+        videoFrames[i].style.width = '100px';
+      }
+    }
+  } else {
+    sharingScreen = false;
+    cameraButton.style.display = 'block';
+    document.getElementById(`user-container-${uid}`).remove();
+    await client.unpublish([localScreenTracks]);
+
+    switchToCamera();
+  }
+};
+
 document.getElementById('camera-btn').addEventListener('click', toggleCamera);
 document.getElementById('mic-btn').addEventListener('click', toggleMic);
+document.getElementById('screen-btn').addEventListener('click', toggleScreen);
 
 joinRoomInit();
